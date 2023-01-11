f04c93401ba324a9bed1de01f8445f9278b7addc
Rajesh Madasu <mr.madasurajesh@gmail.com>
2023-01-11 12:06:52 +0400

    Alertnative way to calling api calls using FutureBuilder and Consumer

--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -14,39 +14,52 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
+  // bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    _isLoading = true;
+    //  _isLoading = true;
     //Future.delayed(Duration.zero).then((_) async {
-    Provider.of<Orders>(context, listen: false)
-        .fetchAndSetOrders()
-        .then((value) => {
-              setState(() {
-                _isLoading = false;
-              })
-            });
+    // Provider.of<Orders>(context, listen: false).fetchAndSetOrders()
+    //     .then((value) => {
+    //           setState(() {
+    //         //    _isLoading = false;
+    //           })
+    //         });
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    //final orderData = Provider.of<Orders>(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) =>
-                  OrderItem(order: orderData.orders[index]),
-              itemCount: orderData.orders.length,
-            ),
+      body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (context, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapshot.error != null) {
+                return const Center(
+                  child: Text('Something went wrong...'),
+                );
+              } else {
+                return Consumer<Orders>(
+                    builder: (ctx, orderData, child) => ListView.builder(
+                          itemBuilder: (ctx, index) =>
+                              OrderItem(order: orderData.orders[index]),
+                          itemCount: orderData.orders.length,
+                        ));
+              }
+            }
+          }),
       drawer: AppDrawer(),
     );
   }
